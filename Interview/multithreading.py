0e510526e7fb8de9e10cc6c17fa89b41c4e5d9bf
Mayur <mayurnaravani@gmail.com>
2023-10-29 22:52:41 +0530

    Update multithreading.py

--- Interview/multithreading.py
+++ Interview/multithreading.py
@@ -1,7 +1,7 @@
 """
 Basic illustration of a multithreading
 
-Scenario: We have a scenario where in a user tries to download a report on the UI. The bigger the report the time it takes, the user will not know whether the process is running or it has stopped. To address the scenario, the implementation was to create a separate thread based on the number of cells in the excel, fox ex: if the number of cells > 2,00,000 then create a separate thread and shoe a msg on the UI. Then later once the thread is completed send the excel file to the email address. 
+Scenario: We have a scenario where in a user tries to download a report on the UI. The bigger the report the time it takes, the user will not know whether the process is running or it has stopped. To address the scenario, the implementation was to create a separate thread based on the number of cells in the excel, fox ex: if the number of cells > 2,00,000 then create a separate thread and shoe a msg on the UI. Then later once the thread is completed send the excel file to the email address.
 """
 
 import threading
